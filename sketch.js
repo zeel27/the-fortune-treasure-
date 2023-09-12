694e8f76fc4d76d5e00931e02bb833dcab0ed9d7
zeel27 <110594630+zeel27@users.noreply.github.com>
2023-09-12 17:24:51 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var splashScreen
+
+function preload(){
+    splashScreen=loadImage("The Fortune Treasure!!.gif") 
+}
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+}
+
+function draw(){
+    background(splashScreen)
+}
